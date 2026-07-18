faef4cd8a31b87fc41bba7e6b4dab49c9fa7153d
Zaijun <thoughtworks.t.shi@extaccount.com>
2026-07-18 17:15:52 +0800

    #00000 tech - init project

---
+++ src/test/kotlin/org/walnut/playground/PlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.walnut.playground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
